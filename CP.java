433533adcd43556acb4d04119f8e4deaee1106a9
vksh-chandra <52464865+vksh-chandra@users.noreply.github.com>
2021-08-10 18:15:30 +0530

    Add files via upload

---
+++ CP.java
@@ -0,0 +1,83 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+public class CP
+{
+    
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		FastReader sc =new FastReader();
+		
+	    int t=sc.nextInt();
+	    
+	    while(t-->0)
+	    {
+         
+         
+      
+
+        
+          
+	    }
+	    
+	}
+	
+	
+	
+	static class FastReader {
+        BufferedReader br;
+        StringTokenizer st;
+ 
+        public FastReader()
+        {
+            br = new BufferedReader(
+                new InputStreamReader(System.in));
+        }
+ 
+        String next()
+        {
+            while (st == null || !st.hasMoreElements()) {
+                try {
+                    st = new StringTokenizer(br.readLine());
+                }
+                catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return st.nextToken();
+        }
+ 
+        int nextInt() { return Integer.parseInt(next()); }
+ 
+        long nextLong() { return Long.parseLong(next()); }
+ 
+        double nextDouble()
+        {
+            return Double.parseDouble(next());
+        }
+        
+        float nextFloat()
+        {
+            return Float.parseFloat(next());
+        }
+ 
+        String nextLine()
+        {
+            String str = "";
+            try {
+                str = br.readLine();
+            }
+            catch (IOException e) {
+                e.printStackTrace();
+            }
+            return str;
+        }
+        
+        int[] readArray(int n) {
+			int[] a=new int[n];
+			for (int i=0; i<n; i++) a[i]=nextInt();
+			return a;
+		}
+	}
+}
